51453c3b8fdd6251d24fbbfd061702c006330458
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-10-27 12:39:58 +0000

    net: tcp: fix rtable leak in tcp_is_local[6]

--- include/torch_cpp/SuperPoint.hpp
+++ include/torch_cpp/SuperPoint.hpp
@@ -1,4 +1,21 @@
 /**
  * @file    SuperPoint.hpp
  *
- * @
+ * @author  btran
+ *
+ */
+
+#pragma once
+
+#include <string>
+
+#include <opencv2/opencv.hpp>
+
+namespace _cv
+{
+class CV_EXPORTS_W SuperPoint : public cv::Feature2D
+{
+ public:
+    struct Param {
+        // reduce input shapes can increase speed and reduce (GPU) memory consumption
+        // at the cost of accurac
