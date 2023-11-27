5669c091d1e42d2979e3aab464b77d45924b76dd
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-11-27 05:29:34 +0000

    Update copyright dates.

--- scripts/jit_superpoint_superglue_models.bash
+++ scripts/jit_superpoint_superglue_models.bash
@@ -6,4 +6,10 @@ git submodule update --init --recursive
 readonly CURRENT_DIR=$(dirname $(realpath $0))
 readonly ROOT_DIR=$(realpath "$CURRENT_DIR"/..)
 
-cd $ROOT_DIR/scripts/superglu
+cd $ROOT_DIR/scripts/superglue/SuperGluePretrainedNetwork
+echo "apply patch" && git apply ../jit_patch.patch
+python3 -m pip install -r $ROOT_DIR/scripts/superglue/SuperGluePretrainedNetwork/requirements.txt --user
+
+cd $ROOT_DIR/data
+python3 $ROOT_DIR/scripts/superglue/jit_superglue_model.py
+python3 $ROOT_DIR/s
