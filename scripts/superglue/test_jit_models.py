ed90ec7be6f02cc4c6169aa7ef744d9693ae1e7c
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-12-01 06:39:37 +0000

    Clean up styles

--- scripts/superglue/test_jit_models.py
+++ scripts/superglue/test_jit_models.py
@@ -1,2 +1,16 @@
 import os
-from typing import
+from typing import List
+
+import cv2
+import numpy as np
+import torch
+
+from SuperGluePretrainedNetwork.models.utils import read_image
+
+
+def get_args():
+    import argparse
+
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("--superpoint", "-sp", type=str, required=True)
+    parser.add_argumen
