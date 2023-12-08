33964fd5107ef0d051fc3e4d9de5a712ab3dcc8f
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-12-08 08:36:40 +0000

    SoC: msm: qdsp6v2: Enable gapless for pcm offload

--- scripts/superglue/test_jit_models.py
+++ scripts/superglue/test_jit_models.py
@@ -17,4 +17,14 @@ def get_args():
     parser.add_argument("--image0", "-i0", type=str, required=True)
     parser.add_argument("--image1", "-i1", type=str, required=True)
     parser.add_argument("--match_threshold", "-m", type=float, default=0.2)
-    parser.add_argument("--keypoint_threshold", 
+    parser.add_argument("--keypoint_threshold", "-k", type=float, default=0.2)
+    parser.add_argument("--remove_borders", "-r", type=int, default=4)
+    parser.add_argument("--nms_radius", "-n", type=int, default=2)
+
+    return parser.parse_args()
+
+
+def main(args):
+    image_paths = [args.image0, args.image1]
+
+    superpoi
