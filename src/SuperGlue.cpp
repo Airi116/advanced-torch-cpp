71282b1d5c914f7579c8d19b4152e39d03ab696b
Airi116 <114504603+Airi116@users.noreply.github.com>
2023-12-31 18:18:51 +0000

    0.22.2

--- src/SuperGlue.cpp
+++ src/SuperGlue.cpp
@@ -2,3 +2,18 @@
  * @file    SuperGlue.cpp
  *
  * @author  btran
+ *
+ */
+
+#include <torch/script.h>
+#include <torch/torch.h>
+
+#include <torch_cpp/SuperGlue.hpp>
+#include <torch_cpp/Utility.hpp>
+
+namespace _cv
+{
+class SuperGlueImpl : public SuperGlue
+{
+ public:
+    explicit SuperGlueImpl(const SuperGlue::Param& param)
