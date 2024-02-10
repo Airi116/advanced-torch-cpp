64d1bb78ee86aa7ab7e4c02db2f3b21a6cb57a6e
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-02-10 17:31:01 +0000

    renamed images

--- src/SuperPoint.cpp
+++ src/SuperPoint.cpp
@@ -15,4 +15,15 @@
 
 namespace
 {
-cv::Mat copyRows(const cv::Mat& sr
+cv::Mat copyRows(const cv::Mat& src, const std::vector<int>& indices);
+}  // namespace
+
+namespace _cv
+{
+class SuperPointImpl : public SuperPoint
+{
+ public:
+    explicit SuperPointImpl(const SuperPoint::Param& param);
+
+    void detectAndCompute(cv::InputArray _image, cv::InputArray _mask, std::vector<cv::KeyPoint>& keyPoints,
+    
