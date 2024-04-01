4a4dd2a228ce2456cb9fa65d088e3d2fb2572873
Airi116 <114504603+Airi116@users.noreply.github.com>
2024-04-01 20:45:39 +0000

    cleaned up spacing

---
+++ tests/TestSuperGlue.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file    TestSuperGlue.cpp
+ *
+ * @author  btran
+ *
+ */
+
+#include <gtest/gtest.h>
+
+#include <torch_cpp/torch_cpp.hpp>
+
+#include "config.h"
+
+TEST(TestSuperGlue, TestInit
